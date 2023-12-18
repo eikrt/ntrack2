d7203f0e62ffb36e771ec5b2f4ad62c3b0e572b5
Eino Korte <e.i.korte@gmail.com>
2023-12-18 15:26:02 +0200

    refactor 1

--- main.py
+++ main.py
@@ -18,92 +18,108 @@ def play_audio(audio_file):
 def kill_list(ps):
     for p in ps:
         p.terminate()
+def terminate_list(ps):
+    for p in ps:
+        p.terminate()
+def new_page(state):
+    if len(state['pages'])-1 < state['current_page']:
+        state['pages'].append(state['current_rec_processes'].copy())
+        state['current_rec_processes'] = []
+    else:
+        state['current_rec_processes'] = state['pages'][state['current_page']]
+def update(state):
+    if len(state['current_rec_processes']) > 0:
+        state['last_rec'] = state['current_rec_processes'][len(state['current_rec_processes'])-1] 
+    if len(state['play_processes']) > 0:
+        state['last_play'] = state['play_processes'][len(state['play_processes'])-1] 
+    if state['last_rec'] != None:
+        state['last_rec'].terminate()
+def play(state):
+    if state['current_track']> 0:
+
+        cpage = state['current_page']
+        ctrack = state['current_track'] - 1
+        state['play_processes'].append(play_audio(f'recordings/project/page-{cpage}-track-{ctrack}.wav'))
+        
+def record(state):
+    if state['mode'] != 'cycle':
+        state['current_track'] += 1
+        cpage = state['current_page']
+        ctrack = state['current_track']
+        file_name = f'recordings/project/page-{cpage}-track-{ctrack}.wav'
+        print(f'Recording track {ctrack}')
+        state['current_rec_processes'].append(record_audio(file_name))
+def handle_input(s, state):
+    if s == 'q':
+        return;
+    elif s == 'n':
+        pass
+    elif s == '+':
+        state['mode'] = 'cycle'
+        state['current_track'] += 1
+    elif s == '-':
+        state['current_track'] -= 1
+        state['mode'] = 'cycle'
+    elif s == 'np':
+        state['mode'] = 'cycle'
+        state['index'] = 0
+        state['current_page'] += 1
+        kill_list(state['play_processes'])
+
+    elif s == 'pp':
+        state['mode'] = 'cycle'
+        state['current_track'] = 0
+        kill_list(state['play_processes'])
+        if state['current_page'] > 0:
+            state['current_page'] -= 1
+            state['current_rec_processes'] = state['pages'][state['current_page']] 
+        else:
+            print("Cant't go beyond page 1!")
+    elif s == 'c':
+        if state['mode'] == 'cycle':
+            state['mode'] = 'rec'
+        else:
+            state['mode'] = 'cycle'
+    elif s == 'clear':
+        kill_list(state['play_processes'])
+        state['play_processes'] = []
+        state['last_rec'].terminate()
+        state['current_rec_processes'] = []
+        state['current_track'] = 0
+        return 
+    elif s == 'u':
+        if len(state['play_processes']) > 0:
+            state['play_processes'].pop()
+        state['last_play'].kill()
+        if state['current_track'] > 1:
+            state['current_track']-= 2
+        else:
+            state['current_track'] = 0
+            print("Can't go to track 0!")
+            return 
+    elif s == 'p':
+        print("Press enter to resume")
+        kill_list(state['current_rec_processes'])
+        input()
+    elif s == 'm':
+        state['muted'] = not state['muted'] 
+        kill_list(state['play_processes'])
 if __name__ == "__main__":
-    index = 0
-    current_page = 0
-    pages = []
-    current_rec_processes = []
-    play_processes = []
-    last_rec = None
-    last_play = None
-    mode = 'rec'
-    muted = False
+    state = {'current_track': 0, 'current_page': 0, 'pages': [], 'current_rec_processes': [], 'play_processes': [], 'last_rec': None, 'last_play': None, 'mode': 'rec', 'muted': False}
     while True:
         print("Press enter to record! ")
-        print(f'Currently on page {current_page}, track: {index}!')
-        print(f'MODE: {mode}')
-        print(f'MUTED: {muted}')
+        cpage = state['current_page']
+        ctrack = state['current_page']
+        m = state['mode']
+        mu = state['muted']
+        print(f'Currently on page {cpage}, track: {ctrack}!')
+        print(f'MODE: {m}')
+        print(f'MUTED: {mu}')
         s = input()
-        if len(current_rec_processes) > 0:
-            last_rec = current_rec_processes[len(current_rec_processes)-1] 
-        if len(play_processes) > 0:
-            last_play = play_processes[len(play_processes)-1] 
-        if last_rec != None:
-            last_rec.terminate()
-        prev_file_name = f'recordings/project/page-{current_page}-track-{index}.wav'
-        if s == 'q':
-            break;
-        elif s == 'n':
-            pass
-        elif s == '+':
-            mode = 'cycle'
-            index += 1
-        elif s == '-':
-            index -= 1
-            mode = 'cycle'
-        elif s == 'np':
-            mode = 'cycle'
-            index = 0
-            current_page += 1
-            for p in play_processes:
-                p.kill()
-            if len(pages)-1 < current_page:
-                pages.append(current_rec_processes.copy())
-                current_rec_processes = []
-            else:
-                current_rec_processes = pages[current_page]
-        elif s == 'pp':
-            mode = 'cycle'
-            index = 0
-            kill_list(play_processes)
-            if current_page > 0:
-                current_page -= 1
-                current_rec_processes = pages[current_page] 
-            else:
-                print("Cant't go beyond page 1!")
-        elif s == 'c':
-            if mode == 'cycle':
-                mode = 'rec'
-            else:
-                mode = 'cycle'
-        elif s == 'clear':
-            kill_list(play_processes)
-            play_processes = []
-            last_rec.terminate()
-            current_rec_processes = []
-            index = 0
-            continue
-        elif s == 'u':
-            if len(play_processes) > 0:
-                play_processes.pop()
-            last_play.kill()
-            if index > 1:
-                index -= 2
-            else:
-                index = 0
-                print("Can't go to track 0!")
-                continue
-        elif s == 'p':
-            print("Press enter to resume")
-            kill_list(current_rec_processes)
-            input()
-        elif s == 'm':
-            muted = not muted 
-            kill_list(play_processes)
-        if index > 0:
-            play_processes.append(play_audio(prev_file_name))
-        if mode != 'cycle':
-            index += 1
-            file_name = f'recordings/project/page-{current_page}-track-{index}.wav'
-            print(f'Recording track {index}!')
-            current_rec_processes.append(record_audio(file_name))
+        # update temp values etc
+        update(state)
+        prev_file_name = f'recordings/project/page-{cpage}-track-{ctrack}.wav'
+        # handle input
+        handle_input(s, state)
+        play(state)
+        record(state)
